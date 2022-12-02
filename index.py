dd955a99dd08392829ddc7dc837438ccd5152b72
Mona Lisa <holmesmainsend@gmail.com>
2022-12-02 17:45:30 -0500

    variable renaming

--- index.py
+++ index.py
@@ -7,15 +7,15 @@ while page < 600:
     url='https://www.ebay.com/sch/i.html?_from=R40&_nkw=book&_sacat=0&_pgn={page}'
     response = requests.get(url)
 
-    #Extract book titles on current page
-    soup = BeautifulSoup(response.text, 'html.parser')  # Parse HTML
-    bookTitles = soup.findAll("span", role = "heading")  # makes list of book titles
+    #Extract listing titles on current page
+    soup = BeautifulSoup(response.text, 'html.parser')   # Parse HTML
+    listingTitles = soup.findAll("span", role = "heading")  # makes list of book titles
 
     # Prints all titles on current page
     print("############### Current page number: " + str(page) + " ###############")
     print()
-    for bookTitle in bookTitles:
-        print(bookTitle.text)
+    for listingTitle in listingTitles:
+        print(listingTitle.text)
         print()
     
-    page = page + 1
+    page += 1
