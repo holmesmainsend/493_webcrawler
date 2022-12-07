1792fe02391ecca139be4c9bf5c3b773464ae67d
sdog2000 <39376813+sdog2000@users.noreply.github.com>
2022-12-07 00:40:09 -0500

    Working CSV and Location testing
    CSV works (mostly). working on implementation of rest of CT cars and trucks sections

--- crawler.py
+++ crawler.py
@@ -6,58 +6,57 @@ from bs4 import BeautifulSoup
 #from random import randint
 #from IPython.core.display import clear_output
 import pandas as pd
-
+locations = ['hartford','newlondon','newhaven','nwct']
+locationsIndex = 0
 rp = urllib.robotparser.RobotFileParser()
 rp.set_url("https://www.craigslist.org/robots.txt")
 rp.read()
 rrate = rp.request_rate("*")
-#print(rrate.requests)
-#print(rrate.seconds)
 rp.crawl_delay("*")
 pagen= 0
-page = 0
 newlstt = []
 newlstp=[]
-#s = input("Enter a search: ")
-url = 'https://hartford.craigslist.org/search/sss?query=car'
+#s = input("Enter a keyword: ")
+url = 'https://'+locations[locationsIndex]+'.craigslist.org/search/cta'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')  # Parse HTML
 totalItems = soup.find("span", class_ = 'totalcount')
-while pagen < int(totalItems.text):
-    # sets up starting website (eBay search for "book", starting with second page of results)
+while locationsIndex < 3:
+    while pagen < int(totalItems.text):
+        # sets up starting website (craigslist cars+trucks section, starting with first page of results)
 
-    if rp.can_fetch("*", url):
-        response = requests.get(url)
+        if rp.can_fetch("*", url):
+            response = requests.get(url)
 
-        # Extract listing titles on current page
-        soup = BeautifulSoup(response.text, 'html.parser')  # Parse HTML
-        #listingLinks = soup.findAll("h3", class_="result-heading")
+            # Extract listing titles on current page
+            soup = BeautifulSoup(response.text, 'html.parser')  # Parse HTML
+            #listingLinks = soup.findAll("h3", class_="result-heading")
 
-        listingTitles = soup.findAll("a", class_= 'result-title hdrlnk')
-        listingPrices = soup.findAll("span", class_="result-price")
+            listingTitles = soup.findAll("a", class_= 'result-title hdrlnk')
+            listingPrices = soup.findAll("span", class_="result-price")
 
-        # Prints all titles on current page
-        #print("############### Current page number: " + str(page) + " ###############")
-        #print()
-        for listingTitle in listingTitles:
-            print(listingTitle.text)
-            print()
-            newlstt.append(listingTitle.text)
-        for x in range(0,len(listingTitles)):
-            print(listingPrices[x].text)
-            newlstp.append(listingPrices[x].text)
-       # for listingLink in listingLinks:
-        #    print(listingLink)
+            # Prints all titles on current page
+            for y in range(0,len(listingTitles)-1):
+                print(listingTitles[y].text)
+                if listingTitles[y+1].text == listingTitles[y].text:
+                    continue
+                else: newlstt.append(listingTitles[y].text)
 
-    item_data = pd.DataFrame({'title': newlstt, 'price': newlstp})
+            for x in range(0,len(listingPrices)-1):
+                print(listingPrices[x].text)
+                if listingPrices[x+1].text == listingPrices[x].text:
+                    continue
+                else: newlstp.append(listingPrices[x].text)
+        while len(newlstt) < len(newlstp):
+            newlstt.append('')
+        while len(newlstp) < len(newlstt):
+            newlstp.append('')
 
-    pagen += 120
-    page += 1
-    url = 'https://hartford.craigslist.org/search/sss?s={pagen}&query=cars'
+        pagen += 120
 
-
-print(len(newlstt), len(newlstp))
-print(item_data.info())
-item_data.tail(10)
-
+        url = 'https://'+locations[locationsIndex]+'.craigslist.org/search/cta?s={pagen}'
+    locationsIndex += 1
+    url = 'https://'+locations[locationsIndex]+'.craigslist.org/search/cta'
+    pagen = 0
+item_data = pd.DataFrame({'title': newlstt, 'price': newlstp})
 item_data.to_csv('item_data_raw.csv')
